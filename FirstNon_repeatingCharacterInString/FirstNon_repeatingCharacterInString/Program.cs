99b120fc272b66f3e4e6100555b06736547d4b75
Waleed Naveed <waleednaveed_11@hotmail.com>
2020-02-16 01:55:21 +0500

    Added logic to find first non repeating character in a string

---
+++ FirstNon_repeatingCharacterInString/FirstNon_repeatingCharacterInString/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstNon_repeatingCharacterInString
+{
+    class Program
+    {
+        static Dictionary<char, int> charCountMapping = new Dictionary<char, int>();
+
+        static void FillDictionary(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (charCountMapping.ContainsKey(str[i]))
+                    charCountMapping[str[i]]++;
+                else
+                    charCountMapping.Add(str[i], 1);
+            }
+        }
+
+        static char? GetFirstNonRepeatingCharacter(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                return null;
+            FillDictionary(str);
+            return charCountMapping.Where(x => x.Value == 1).FirstOrDefault().Key;
+        }
+
+        static void Main(string[] args)
+        {
+            string str = "WaleedNaveed";
+
+            char? firstNonRepeatingChar = GetFirstNonRepeatingCharacter(str);
+
+            if (firstNonRepeatingChar.HasValue)
+                Console.WriteLine("First Non-Repeating character is : " + firstNonRepeatingChar.Value);
+            else
+                Console.WriteLine("Either string is null or there is no non-repeating character");
+
+            Console.ReadKey();
+        }
+    }
+}
